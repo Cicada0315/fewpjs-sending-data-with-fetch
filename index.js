4c6cb007489c1cefbc59c4f8d1ce68b5a8647ea1
jean <jin.ahn.1027@gmail.com>
2021-05-09 13:31:15 -0400

    commit tree e7c7a9e4e6ef7365c89281970d0082b54392c186

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    };
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    
+    return fetch("http://localhost:3000/users", configObj)
+        .then(resp => resp.json())
+        .then(results => {
+            const body = document.querySelector("body");
+            const h2 = document.createElement("h2");
+            h2.innerHTML = results.id;
+            body.appendChild(h2);
+        })
+        .catch(error => {
+            const body = document.querySelector("body");
+            const h2 = document.createElement("h2");
+            h2.innerHTML = error.message;
+            body.appendChild(h2);
+        })
+}
+  
